01c02347c603e61536dac5f15fd7d74fedeb2593
znakoa <zhougui@zjwykjyxgs13.wecom.work>
2024-08-08 17:21:55 +0800

    feat: 添加大屏

---
+++ src/pages/bigscreen/IndexPage/layout.tsx
@@ -0,0 +1,39 @@
+import { useEffect, useRef } from 'react'
+import { LayoutStyle, IndexPageStyle, IndexPageContent } from './style'
+import {previewFitScale} from "@site/src/utils/previewScale";
+import TopPage from "@site/src/pages/bigscreen/TopPage";
+import {LeftPageIndex} from "@site/src/pages/bigscreen/LeftPage";
+import {RightPageIndex} from "@site/src/pages/bigscreen/RightPage";
+import {CenterPageIndex} from "@site/src/pages/bigscreen/CenterPage";
+
+
+
+// 总页面
+export const LayoutPage = () => {
+  const scaleDom = useRef<HTMLDivElement>(null)
+  useEffect(() => {
+    const { calcRate, windowResize, unWindowResize } = previewFitScale(
+      1920,
+      1080,
+      scaleDom.current
+    )
+    calcRate()
+    windowResize()
+    return () => {
+      unWindowResize()
+    }
+  }, [])
+
+  return (
+    <LayoutStyle ref={scaleDom} className="scale-layout">
+       <IndexPageStyle>
+      <TopPage />
+        <IndexPageContent>
+          <LeftPageIndex />
+          <CenterPageIndex />
+          <RightPageIndex />
+        </IndexPageContent>
+       </IndexPageStyle>
+    </LayoutStyle>
+  )
+}
---
+++ src/pages/bigscreen/index.tsx
@@ -0,0 +1,20 @@
+// import { Iconstyle } from '/icon/iconfont'
+import { Globalstyle } from './style/global'
+import {LayoutPage} from "@site/src/pages/bigscreen/IndexPage/layout";
+export default function Bigscreen() {
+  return (
+    <>
+      <Globalstyle></Globalstyle>
+      <div
+        style={{
+          position: 'absolute',
+          left: '50%',
+          top: '50%',
+          transform: ' translate(-50%, -50%)',
+        }}>
+         <LayoutPage></LayoutPage>
+      </div>
+      )
+    </>
+  )
+}
