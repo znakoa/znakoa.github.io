4e613d89fd63dc234fdc2ab2e03717478aae285b
znakoa <zhougui@zjwykjyxgs13.wecom.work>
2024-08-15 17:32:50 +0800

    refactor: 重构博客

---
+++ src/pages/projectinfo/_components/_data/index.js
@@ -0,0 +1,100 @@
+// import React from 'react'
+// import Link from '@docusaurus/Link'
+//
+// import { Case } from '../ShowcaseCard'
+
+export const cases = [
+    {
+        title: '数据可视化平台',
+        website: 'https://znakoa.github.io/bigscreen/',
+        preview: require('../../images/bgxxx.png').default,
+        description: (
+            <>
+                基于 React、zustand、DataV、ECharts 框架的 " 数据大屏项目 "
+                支持数据动态刷新渲染、屏幕适配、数据请求模拟、局部样式、图表自由替换/复用等功能。 使用了函数式编程
+            </>
+        ),
+        source: 'https://github.com/znakoa/bigscreen',
+        tags: ['zustand', 'react','node'],
+    },
+    // {
+    //   title: '音视频通信解决方案样板间',
+    //   website: '',
+    //   preview: require('./_previews/audio-video-case-room.png').default,
+    //   description: (
+    //     <>
+    //       使用腾讯云开发Cloudbase实现了前端和后端
+    //       <br />
+    //       <b>活动页面</b>: 页面展示 + 上报用户行为 <br />
+    //       <b>管理页面</b>: 登录鉴权 + 文案配置 + 图片/PDF上传 +
+    //       图表展示分析用户行为
+    //     </>
+    //   ),
+    //   source: '',
+    //   tags: ['Tencent', 'react'],
+    // },
+    // {
+    //   title: '视立方·音视频终端引擎 - 购买页',
+    //   website: '',
+    //   preview: require('./_previews/vcube-buy.png').default,
+    //   description: (
+    //     <>
+    //       腾讯云视立方资源包购买页, 支持多种<b>不同类别</b>的商品同时购买,
+    //       本地持久化购物车商品
+    //     </>
+    //   ),
+    //   source: '',
+    //   tags: ['Tencent', 'react'],
+    // },
+    // {
+    //   title: '腾讯云·控制台 - 云直播/视频处理',
+    //   website: '',
+    //   preview: require('./_previews/live-console.png').default,
+    //   description: (
+    //     <>
+    //       - 重构云直播计费用量页面
+    //       <br />- 实现了 '拉流转推' 统计数据展示页面
+    //       <br />- 实现了 视频处理控制台的一些小需求
+    //     </>
+    //   ),
+    //   source: '',
+    //   tags: ['Tencent', 'react'],
+    // },
+    // {
+    //   title: 'fhfhockey.com',
+    //   website: '',
+    //   preview: require('./_previews/fhfhockey.com-player-card.png').default,
+    //   description: (
+    //     <>
+    //       分析北美冰球队伍
+    //       <br />- SEO, 性能优化/监控, 服务器端渲染
+    //       <br />- Serverless function
+    //       <br />- Chart.js 图表
+    //       <br />- 响应式设计
+    //       <br />- monorepo
+    //     </>
+    //   ),
+    //   source: '',
+    //   tags: ['production', 'react', 'next.js'],
+    // },
+    // {
+    //   title: '今日小条',
+    //   website: '',
+    //   preview: require('./_previews/xiaotiao.png').default,
+    //   description:
+    //     '模仿 "今日头条" web 的PC端和移动端的网站. 响应式设计/Puppeteer 爬取的头条新闻数据',
+    //   source: '',
+    //   tags: ['personal', 'react', 'material-ui'],
+    // },
+    // {
+    //   title: '金铲铲之战-资料库',
+    //   website: '',
+    //   preview: require('./_previews/jcc.png').default,
+    //   description:
+    //     '掌上英雄联盟 - 金铲铲之战-资料库. 实现时空裂痕-装备分页和装备筛选',
+    //   source: '',
+    //   tags: ['interview', 'react', 'mobile'],
+    // },
+]
+
+export default cases
