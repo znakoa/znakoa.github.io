1b557ab3095790e4e64766482c502e5f08b22256
znakoa <zhougui@zjwykjyxgs13.wecom.work>
2024-08-16 08:49:55 +0800

    feat: 首页优化

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,23 +10,27 @@ import styles from './index.module.css';
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-        <div className="container">
-            <img
-                src={require('@site/static/img/hello-world.gif').default}
-                style={{flex: '0 0 auto', borderRadius: 8}}
-                alt={'背景'}
-            />
-            <Heading as="h1" className="hero__title" style={{ marginTop: '100px' }}>
-                遇上即是上上签
-            </Heading>
-            <div className={styles.buttons}>
-                <Link className="button button--secondary button--lg" to="/about">
-                    关于作者
-                </Link>
-            </div>
-        </div>
-    </header>
+      <header className={clsx('hero--primary', styles.heroBanner)}>
+          <div className="container" style={{paddingBottom:'20px'}}>
+              <img
+                  src={require('@site/static/img/hello-world.gif').default}
+                  style={{flex: '0 0 auto', borderRadius: 8}}
+                  alt={'背景'}
+              />
+          </div>
+
+          <div className="container">
+
+              <Heading as="h1" className="hero__title" >
+                  遇上即是上上签
+              </Heading>
+              <div className={styles.buttons}>
+                  <Link className="button button--secondary button--lg" to="/about">
+                      关于作者
+                  </Link>
+              </div>
+          </div>
+      </header>
   );
 }
 
