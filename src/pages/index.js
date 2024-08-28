15780607c7704b49a0d6c2cf77141bd18685f2fc
znakoa <zhougui@zjwykjyxgs13.wecom.work>
2024-08-28 15:09:19 +0800

    feat:22

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,13 @@
 import clsx from 'clsx'
 import Link from '@docusaurus/Link'
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext'
 import Layout from '@theme/Layout'
-import ParticlesBg from 'particles-bg'
-// import BrowserOnly from '@docusaurus/BrowserOnly'
-// import HomepageFeatures from '@site/src/components/HomepageFeatures';
 
 import Heading from '@theme/Heading'
 import styles from './index.module.css'
 
 function HomepageHeader() {
-  const { siteConfig } = useDocusaurusContext()
   return (
     <header className={clsx('hero--primary', styles.heroBanner)}>
-      {/* <BrowserOnly>
-        {() => <ParticlesBg type="random" bg={true}></ParticlesBg>}
-      </BrowserOnly> */}
-
       <div className="container" style={{ paddingBottom: '20px' }}>
         <img
           src={require('@site/static/img/hello-world.gif').default}
