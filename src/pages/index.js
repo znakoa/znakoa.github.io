7b8a86da0f98ea08492e5fe9a07632f089b91391
znakoa <zhougui@zjwykjyxgs13.wecom.work>
2024-08-28 15:06:22 +0800

    feat：11

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import Link from '@docusaurus/Link'
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext'
 import Layout from '@theme/Layout'
 import ParticlesBg from 'particles-bg'
-import BrowserOnly from '@docusaurus/BrowserOnly'
+// import BrowserOnly from '@docusaurus/BrowserOnly'
 // import HomepageFeatures from '@site/src/components/HomepageFeatures';
 
 import Heading from '@theme/Heading'
@@ -13,9 +13,9 @@ function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext()
   return (
     <header className={clsx('hero--primary', styles.heroBanner)}>
-      <BrowserOnly>
+      {/* <BrowserOnly>
         {() => <ParticlesBg type="random" bg={true}></ParticlesBg>}
-      </BrowserOnly>
+      </BrowserOnly> */}
 
       <div className="container" style={{ paddingBottom: '20px' }}>
         <img
