fa7c9c3c9139edb9bf5a19aa56310f391d16498c
zhougui <2558181338@qq.com>
2025-06-10 14:24:03 +0800

    build(docusaurus): 添加本地搜索功能并支持中英文
    - 在 docusaurus.config.ts 中添加了 @easyops-cn/docusaurus-search-local 主题
    - 配置了本地搜索的选项，包括启用哈希索引和中英文语言支持
    - 在 package.json 中添加了 @easyops-cn/docusaurus-search-local 依赖

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -76,6 +76,23 @@ const config: Config = {
       } satisfies Preset.Options,
     ],
   ],
+  themes:[
+      [
+        require.resolve("@easyops-cn/docusaurus-search-local"),
+        /** @type {import("@easyops-cn/docusaurus-search-local").PluginOptions} */
+        ({
+          // ... Your options.
+          // `hashed` is recommended as long-term-cache of index file is possible.
+          hashed: true,
+
+          // For Docs using Chinese, it is recomended to set:
+           language: ["en", "zh"],
+
+          // If you're using `noIndex: true`, set `forceIgnoreNoIndex` to enable local index:
+          // forceIgnoreNoIndex: true,
+        }),
+      ]
+  ],
 
   themeConfig: {
     // Replace with your project's social card
