d434ccbf3582fefdb481335110665e82c37fcca4
zhougui <2558181338@qq.com>
2025-10-29 11:07:26 +0800

    chore(config): 更新项目配置和部署工作流
    - 将 GitHub 组织名从 facebook 更改为 znakoa
    - 将项目名从 docusaurus 更改为 Website
    - 升级 GitHub Actions checkout 动作至 v4 版本
    - 升级 GitHub Actions setup-node 动作至 v4 版本- 更换部署动作至 peaceiris/actions-gh-pages@v3
    - 调整部署配置参数以匹配新动作要求

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -22,8 +22,8 @@ const config: Config = {
 
     // GitHub pages deployment config.
     // If you aren't using GitHub pages, you don't need these.
-    organizationName: 'facebook', // Usually your GitHub org/user name.
-    projectName: 'docusaurus', // Usually your repo name.
+    organizationName: 'znakoa', // Usually your GitHub org/user name.
+    projectName: 'Website', // Usually your repo name.
 
     onBrokenLinks: 'throw',
     themes:[
